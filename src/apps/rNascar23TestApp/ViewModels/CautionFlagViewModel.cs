13efd7537ce6dc7a1b5ccf840952aef6d60752d4
Rob Roberts <rroberts@centeredgesoftware.com>
2023-03-20 15:01:26 -0400

    Added cautions grid

---
+++ src/apps/rNascar23TestApp/ViewModels/CautionFlagViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace rNascar23TestApp.ViewModels
+{
+    internal class CautionFlagViewModel
+    {
+        public int LapNumber { get; set; }
+        public string Comment { get; set; }
+        public string Beneficiary { get; set; }
+    }
+}
